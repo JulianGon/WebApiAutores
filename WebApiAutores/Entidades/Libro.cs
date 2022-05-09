5da7ddff057563f3fb177a798c2923740e7fa67a
Julián González <juliangonzalezhuertas@gmail.com>
2022-05-09 19:16:01 +0200

    Añado Libros como tabla relacionada con Autores y creamos su Get y su Post

---
+++ WebApiAutores/Entidades/Libro.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+        public Autor  Autor { get; set; } // Propiedad de Navegación -> Habilita cargar desde un libro los datos del autor 
+
+    }
+}
